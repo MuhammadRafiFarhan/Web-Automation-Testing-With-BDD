76d9bb6ee28bc02577d2ce94b85c888f644fcb4d
Rachmat Purwa Saputra <rachmat.purwa.tif421@polban.ac.id>
2024-05-12 18:23:37 +0700

    Restructuring the project folder

---
+++ src/test/java/com/swaglab/TestRunner.java
@@ -0,0 +1,19 @@
+package com.swaglab;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.FEATURES_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "classpath:com/swaglab/features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.swaglab.steps")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty,junit:target/cucumber-reports/Cucumber.xml,json:target/cucumber-reports/Cucumber.json,html:target/cucumber-reports/Cucumber.html,timeline:target/cucumber-reports/CucumberTimeline")
+public class TestRunner {
+    
+}
---
+++ src/test/java/com/swaglab/steps/LoginSteps.java
@@ -0,0 +1,82 @@
+package com.swaglab.steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginSteps {
+    private WebDriver driver;
+
+    @Before
+    public void initializeDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @Given("I have opened the browser")
+    public void I_have_opened_the_browser() {
+        
+    }
+
+    @When("I open Swaglabs website")
+    public void I_open_Swaglabs_website() {
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @Then("username box should exist")
+    public void username_box_should_exist() {
+        assertTrue(driver.findElement(By.id("user-name")).isDisplayed());
+    }
+
+    @Then("password box should exist")
+    public void password_box_should_exist() {
+        assertTrue(driver.findElement(By.id("password")).isDisplayed());
+    }
+
+    @Then("login button should exist")
+    public void login_button_should_exist() {
+        assertTrue(driver.findElement(By.id("login-button")).isDisplayed());
+    }
+
+    @Given("I am on the Swag Labs login page")
+    public void I_am_on_the_Swag_Labs_login_page() {
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @When("I enter the username {string}")
+    public void I_enter_the_username(String username) {
+        driver.findElement(By.id("user-name")).sendKeys(username);
+    }
+
+    @And("I enter the password {string}")
+    public void I_enter_the_password(String password) {
+        driver.findElement(By.id("password")).sendKeys(password);
+    }
+
+    @And("I click on the login button")
+    public void I_click_on_the_login_button() {
+        driver.findElement(By.id("login-button")).click();
+    }
+
+    @Then("I should be taken to the products page")
+    public void I_should_be_taken_to_the_products_page() {
+        String url = driver.getCurrentUrl();
+        assertTrue(url.contains("inventory.html"));
+    }
+
+    @Then("I should see an error message {string}")
+    public void I_should_see_an_error_message(String errorMessage) {
+        String actualErrorMessage = driver.findElement(By.cssSelector("[data-test='error']")).getText();
+        assertTrue(actualErrorMessage.equals(errorMessage));
+    }
+
+    @After
+    public void closeBrowser() {
+        driver.quit();
+    }
+}
